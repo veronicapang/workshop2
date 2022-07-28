bcf8897e66eb380929cdbb099e15a6957d0e9552
MING YAN PANG <veronica.pang@griffithuni.edu.au>
2022-07-28 16:18:10 +1000

    test commit

--- histogram.py
+++ histogram.py
@@ -1,12 +1,31 @@
 import sys
 import string
 
-yourinput = sys.stdin.read()
-print("Input of histogram.py file:")
+# yourinput = sys.stdin.read().split()
+# print("Input of histogram.py file:")
+# print(yourinput)
+
+
+
+yourinput = sys.stdin.readlines()
+# print("Input of histogram.py file:")
+# print(yourinput[0])
 print(yourinput)
-# print("TEST\t[***]")
-# print("IS\t[***]")
+
+total = 0
+dictionary = dict()
+
+for i in yourinput:
+    line = i.strip().split()
+    word = line[0]
+    count = line[1]
+    dictionary[word] = count
+    total += int(count)
+
+for word in dictionary.keys():
+    count = int(dictionary[word])
+    print(word + "\t" + '[' + "**" * count + ']\t' + str(round((count / total * 100))) + '%')
 
 
 
-# *2   average
+# *2 , average
