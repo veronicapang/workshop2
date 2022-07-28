cf33cc27c3d3c471c95785e7f90b8ce46a0f338b
MING YAN PANG <veronica.pang@griffithuni.edu.au>
2022-07-28 15:44:08 +1000

    test commit

---
+++ histogram.py
@@ -0,0 +1,12 @@
+import sys
+import string
+
+yourinput = sys.stdin.read()
+print("Input of histogram.py file:")
+print(yourinput)
+# print("TEST\t[***]")
+# print("IS\t[***]")
+
+
+
+# *2   average
